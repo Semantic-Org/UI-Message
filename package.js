4529a56573fc2f80c36463cb134c8688f0ce89e1
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:14:15 -0500

    Updated component to version 1.9.0

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-message',
+  summary : 'Semantic UI - Message (official): Single component release of message',
+  version : '1.9.0',
+  git     : 'git://github.com/Semantic-Org/UI-Message.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
