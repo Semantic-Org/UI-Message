5b3d6d6ff38d304f81e0d1520da2b395933141f5
Pusher Robot <jack@semantic-ui.com>
2015-02-20 17:15:59 -0500

    Updated component to version 1.9.3

--- package.js
+++ package.js
@@ -1,17 +1,14 @@
-var
-  where = 'client' // Adds files only to the client
-;
 
 Package.describe({
   name    : 'semantic:ui-message',
-  summary : 'Semantic UI - Message (official): Single component release of message',
-  version : '1.9.2',
+  summary : 'Semantic UI - Message: Single component release',
+  version : '1.9.3',
   git     : 'git://github.com/Semantic-Org/UI-Message.git',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.addFiles([
-    
-  ], where);
+    'message.css'
+  ], 'client');
 });
