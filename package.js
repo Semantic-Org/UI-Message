fad18e393727e64d57550c8f45567909fc08fc2e
Pusher Robot <jack@semantic-ui.com>
2016-08-21 23:33:50 -0400

    Updated component to version 2.2.3

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-message',
   summary : 'Semantic UI - Message: Single component release',
-  version : '2.2.2',
+  version : '2.2.3',
   git     : 'git://github.com/Semantic-Org/UI-Message.git',
 });
 
