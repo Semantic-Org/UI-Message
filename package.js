f2e52c9436e4c7460f52c368540c31ce45c5b063
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:42:09 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-message',
   summary : 'Semantic UI - Message: Single component release',
-  version : '2.2.8',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Message.git',
 });
 
